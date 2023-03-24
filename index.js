216cbf4a0d5e97958594183b56ad5293cd9939e5
Pedro Piastrele <ppiastrele@gmail.com>
2023-03-24 09:33:21 -0300

    Update src folder name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Client, GatewayIntentBits } from 'discord.js';
-import { demotivationalQuotes } from './helper/demotivationalQuotes.js';
+import { demotivationalQuotes } from './src/demotivationalQuotes.js';
 
 const client = new Client({
   intents:[
